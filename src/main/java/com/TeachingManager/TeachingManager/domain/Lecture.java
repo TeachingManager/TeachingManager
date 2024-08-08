7d8180bcf88394e1710c820fbd54a81c04b1131f
hyeonee <yhe01152@naver.com>
2024-08-08 16:32:30 +0900

    [fix] Lecture 스키마 변경

--- src/main/java/com/TeachingManager/TeachingManager/domain/Lecture.java
+++ src/main/java/com/TeachingManager/TeachingManager/domain/Lecture.java
@@ -18,30 +18,47 @@ public class Lecture {
 
     @Column(name = "name", nullable = false)
     private String name;
-//    private LocalTime startTime;
-//    private LocalTime endTime;
+
+    @Column(name = "category", nullable = false)
     private String category;
+
+    @Column(name = "grade", nullable = false)
     private String grade;
+
+    @Column(name = "fee", nullable = false)
     private int fee;
 
+    @Column(name = "time", nullable = false)
+    private String time;
+
+    // 학원과의 일대다 관계
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "institute_id", nullable = false)
-    private Institute teacher;
+    private Institute institute;
+
+    // 강의와의 일대다 관계
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "teacher_id", nullable = false)
+    private Teacher teacher;
 
 
-//    @Builder
-//    public Lecture(Long lecture_id, String name, LocalTime){
-//        this.title = title;
-//        this.start_date = start_date;
-//        this.end_date = end_date;
-//        this.memo = memo;
-//        this.institute = institute;
-//    }
-//
-//    public void update(String title, LocalTime start_date, LocalTime end_date, String memo) {
-//        this.title = title;
-//        this.start_date = start_date;
-//        this.end_date = end_date;
-//        this.memo = memo;
-//    }
+    @Builder
+    public Lecture(String name, String category, String grade, int fee, String time, Institute institute, Teacher teacher){
+        this.name = name;
+        this.category = category;
+        this.grade = grade;
+        this.fee = fee;
+        this.time= time;
+        this.institute = institute;
+        this.teacher = teacher;
+    }
+
+    public void update(String name, String category, String grade, int fee, String time) {
+        this.name = name;
+        this.category = category;
+        this.grade = grade;
+        this.fee = fee;
+        this.time= time;
+    }
+
 }
