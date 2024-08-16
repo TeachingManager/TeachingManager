cdeb59ad851705e60417be4c39a109efdd22b287
Donggyeom <kdk4344@naver.com>
2024-08-16 22:26:35 +0900

    Lecture time response 형식 변환

--- src/main/java/com/TeachingManager/TeachingManager/DTO/Lecture/LectureResponse.java
+++ src/main/java/com/TeachingManager/TeachingManager/DTO/Lecture/LectureResponse.java
@@ -4,6 +4,13 @@ import com.TeachingManager.TeachingManager.domain.Lecture;
 import com.TeachingManager.TeachingManager.domain.Teacher;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.json.simple.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @NoArgsConstructor
 @Getter
@@ -15,7 +22,10 @@ public class LectureResponse {
     private String grade;
     private int fee;
     private String time;
-    private Teacher teacher;
+    private List<Map<String, String>> convetedTime;
+    private List<JSONObject> jsonTime;
+    // 학원 외래키 등록 후 테스트
+//    private Teacher teacher;
 
     public LectureResponse(Lecture lecture) {
         this.id = lecture.getLecture_id();
@@ -24,7 +34,68 @@ public class LectureResponse {
         this.grade = lecture.getGrade();
         this.fee = lecture.getFee();
         this.time = lecture.getTime();
-        this.teacher = lecture.getTeacher();
+//        this.teacher = lecture.getTeacher();
+        this.convetedTime = ConvertTime(lecture.getTime());
+        this.jsonTime = ConvertJsonTime(lecture.getTime());
+    }
+
+
+    public List<Map<String, String>> ConvertTime(String time) {
+
+        List<Map<String, String>> ct = new ArrayList<>();
+        String[] split = time.split(",");
+        for (String s : split) {
+            String[] arr = s.split("-");
+            Map<String, String> convertedTimes = new HashMap<>();
+            convertedTimes.put("day", ToKorean(arr[0]));
+            convertedTimes.put("time", arr[1]);
+            ct.add(convertedTimes);
+        }
+        return ct;
+    }
+
+    public List<JSONObject> ConvertJsonTime(String time) {
+
+        List<JSONObject> result = new ArrayList<>();
+        String[] split = time.split(",");
+        for(String s : split) {
+            String[] arr = s.split("-");
+            JSONObject json = new JSONObject();
+            json.put("day", ToKorean(arr[0]));
+            json.put("time", arr[1]);
+            result.add(json);
+        }
+        return result;
+    }
+
+    public String ToKorean(String s) {
+        switch (s) {
+            case "MONDAY":
+                return "월요일";
+            case "TUESDAY":
+                return "화요일";
+            case "WEDNESDAY":
+                return "수요일";
+            case "THURSDAY":
+                return "목요일";
+            case "FRIDAY":
+                return "금요일";
+            case "SATURDAY":
+                return "토요일";
+            case "SUNDAY":
+                return "일요일";
+            default:
+                return "오류";
+        }
+    }
+
+    public String Convert(String time) {
+        String result;
+        String[] split = time.split(",");
+        for(String s : split) {
+
+        }
+        return "a";
     }
 
 }
