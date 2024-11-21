6f7ae55ea331e51ef1917b0b57f05b38af62b0ad
hyeonee <yhe01152@naver.com>
2024-11-22 01:13:46 +0900

    [feat] 선생님은 자신이 이번달 가르치는 학생만 조회 가능

--- src/main/java/com/TeachingManager/TeachingManager/Repository/Student/StudentRepository.java
+++ src/main/java/com/TeachingManager/TeachingManager/Repository/Student/StudentRepository.java
@@ -9,11 +9,11 @@ import java.util.UUID;
 
 public interface StudentRepository {
     List<Student> findByInstitute_Pk(UUID pk);
+    List<Student> findByInstitute_and_teacher_Pk(UUID instituteId, UUID teacherId, Short Year, Short Month);
     Student save(Student student);
 
     Optional<Student> findById(UUID Institute_id, Long student_id);
 
     String deleteById(UUID institute_id, Long student_id);
-
 
 }
